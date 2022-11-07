e29055620a0b42a2b8371950a6bd6cb471f54bf9
Robin Hill <rehill012@gmail.com>
2022-11-06 19:56:44 -0500

    Update scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,6 @@
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
 crontab -l > tmp
-echo "0 */6 * * * sh /var/www/bots/gorgar-speaks/scripts/run.sh" >> tmp
+echo "0 */6 * * * bash /var/www/bots/gorgar-speaks/scripts/run.sh" >> tmp
 crontab tmp
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,3 @@
 cd /var/www/bots/gorgar-speaks/
+source env/bin/activate
 python3 /var/www/bots/gorgar-speaks/gorgar.py --publish
